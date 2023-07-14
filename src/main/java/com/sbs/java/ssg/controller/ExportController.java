fb8540bd046e70589000e0147f2edefac6429617
g2jkj0274 <g2jkj0274@gmail.com>
2023-07-14 10:34:57 +0900

    session 추가 및 수정

---
+++ src/main/java/com/sbs/java/ssg/controller/ExportController.java
@@ -0,0 +1,30 @@
+package com.sbs.java.ssg.controller;
+
+import java.util.Scanner;
+
+import com.sbs.java.ssg.container.Container;
+import com.sbs.java.ssg.service.ExportService;
+
+public class ExportController extends Controller {
+	private Scanner sc;
+	private ExportService exportService;
+
+	public ExportController(Scanner sc) {
+		this.sc = sc;
+		exportService = Container.exportService;
+	}
+	public void doAction(String command, String actionMethodName) {
+		switch ( actionMethodName ) {
+		case "html":
+			doHtml();
+			break;
+		default:
+			System.out.println("존재하지 않은 명령어입니다.");
+			break;
+		}
+	}
+	private void doHtml() {
+		System.out.println("html 생성을 시작합니다.");
+		exportService.makeHtml();
+	}
+}
